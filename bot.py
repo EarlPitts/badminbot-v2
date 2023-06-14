5f6d716ea73943a42f46f53dcd4e7c7aec2c4208
Bendeguz Seres <grafitpor@gmail.com>
2023-06-14 18:48:14 +0200

    Remove a very essential functionality

--- bot.py
+++ bot.py
@@ -59,8 +59,6 @@ async def on_message(message):
     """Pin message containing the poll url"""
     if message.author.id == bot.user.id and 'strawpoll.com' in message.content:
         await message.pin()
-    if message.author.id == ADMIN:
-        await message.add_reaction("\u2764\uFE0F")
 
 @bot.command()
 async def joke(ctx):
